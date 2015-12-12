ae613ed4a7950cb79a0617bc69bdec0fc37beec6
Hannah Ward <Hannah.ward9001@gmail.com>
2015-12-12 16:01:29 +0000

    Added option to run for president

--- app/app.js
+++ app/app.js
@@ -27,3 +27,9 @@ app.directive('script', function() {
         }
     };
 });
+
+app.filter('percentage', ['$filter', function ($filter) {
+  return function (input, decimals) {
+    return $filter('number')(input * 100, decimals) + '%';
+  };
+}]);
--- app/game/game.js
+++ app/game/game.js
@@ -22,16 +22,29 @@ angular.module('LD34.game', ['ngRoute'])
     $scope.demand1 = 30;
     $scope.demand2 = 50;
     $scope.dayLength = 5; 
+    $scope.funding = {president: 0};
+    $scope.allowPresidencyRun = false;
+    $scope.presidencyRun = false;
     $scope.production = {
       quality: 50,
       quantity1: 5,
       quantity2: 3
     }
+    $scope.profit = 0;
+    $scope.getProfit = function() {
+      
+    };
 
     $scope.retail = {
       quantity1: 5,
       quantity2: 3
     };
+    $scope.competitorsMade1 = 0;
+    $scope.competitorsMade2 = 0;
+
+    $scope.ticksPassed = 0;
+    $scope.qualitySum = 0;
+    $scope.qualityAverage = 0;
 
     $scope.update = function() {
       $scope.$broadcast("timer-stop");
@@ -40,7 +53,7 @@ angular.module('LD34.game', ['ngRoute'])
       //UPDATE SUPPLY AND DEMAND
       //If demand will fluctuate randomly
       //Supply can be directly influenced by the player
-
+      console.log($scope.funding.president);
       //Update Prices
       var supplyRatio1 = $scope.supply1 / $scope.demand1;
       var supplyRatio2 = $scope.supply2 / $scope.demand2;
@@ -51,23 +64,35 @@ angular.module('LD34.game', ['ngRoute'])
       $scope.priceArray1 = $scope.priceArray1.splice(1, $scope.dayMax);
       $scope.priceArray2 = $scope.priceArray2.splice(1, $scope.dayMax);
       
-      var newPrice1 = ($scope.priceArray1[$scope.dayMax - 1]) + (Math.random()*5*($scope.supplyRatio1 > 1? -1:1));
-      var newPrice2 = ($scope.priceArray2[$scope.dayMax - 1]) + (Math.random()*5*($scope.supplyRatio1 > 1? -1:1)); 
+      var newPrice1 = ($scope.priceArray1[$scope.dayMax - 1]) + (Math.random()*7*(supplyRatio1 > 1? -1:1));
+      var newPrice2 = ($scope.priceArray2[$scope.dayMax - 1]) + (Math.random()*7*(supplyRatio2> 1? -1:1)); 
       if (newPrice1 <= 1) newPrice1 = 1;
       if (newPrice2 <= 1) newPrice2 = 1;
       $scope.priceArray1.push(newPrice1);
       $scope.priceArray2.push(newPrice2);
 
       //Number of buttons bought
-      var numberBought1 = Math.floor(0.2*Math.exp(Math.E, ($scope.supply1 - $scope.demand1)*Math.random()));
-      var numberBought2 = Math.floor(0.2*Math.exp(Math.E, ($scope.supply2 - $scope.demand2)*Math.random())); 
+      var numberBought1 = 1+Math.floor(0.2*Math.exp(Math.E, ($scope.supply1 - $scope.demand1)*Math.random()));
+      var numberBought2 = 1+Math.floor(0.2*Math.exp(Math.E, ($scope.supply2 - $scope.demand2)*Math.random())); 
       $scope.supply1 -= numberBought1;
       $scope.supply2 -= numberBought2; 
       
+      var numberMade1 = 1+Math.floor(5*Math.random()); 
+      var numberMade2 = 1+Math.floor(5*Math.random());
+      if ($scope.supply1 > $scope.demand1) {
+        numberMade1 = 0;
+      }
+      if ($scope.supply2 > $scope.demand2) {
+        numberMade2 = 0;
+      }
+      $scope.supply1 += numberMade1;
+      $scope.supply2 += numberMade2;
+      $scope.competitorsMade1 = numberMade1;
+      $scope.competitorsMade2 = numberMade2;
       if ($scope.supply1 < 0) $scope.supply1 = 0;
       if ($scope.supply2 < 0) $scope.supply2 = 0;
-      $scope.demand1 -= Math.floor(6*Math.random()*(Math.random()>0.5?1:-1))
-      $scope.demand2 -= Math.floor(6*Math.random()*(Math.random()>0.5?1:-1));
+      $scope.demand1 -= Math.floor(5*Math.random()*(Math.random()>0.5?1:-1))
+      $scope.demand2 -= Math.floor(15*Math.random()*(Math.random()>0.5?1:-1));
       if ($scope.demand1 < 0) $scope.demand1 = 0;
       if ($scope.demand2 < 0) $scope.demand2 = 0;
       var karr = [];
@@ -83,19 +108,40 @@ angular.module('LD34.game', ['ngRoute'])
       //Manufacture buttons
       $scope.button1stock += $scope.production.quantity1 - $scope.retail.quantity1;
       $scope.button2stock += $scope.production.quantity2 - $scope.retail.quantity2;
+      var lastMoney = $scope.money;
       $scope.money -= $scope.production.quantity1 * $scope.getPrice1();
       $scope.money -= $scope.production.quantity2 * $scope.getPrice2();
-      $scope.money += $scope.retail.quantity1 * $scope.getPrice1();
-      $scope.money += $scope.retail.quantity2 * $scope.getPrice2();
-      $scope.supply1 += $scope.retail.quantity1;
-      $scope.supply2 += $scope.retail.quantity2;
+      $scope.money += $scope.retail.quantity1 * $scope.priceArray1[$scope.dayMax];
+      $scope.money += $scope.retail.quantity2 * $scope.priceArray2[$scope.dayMax];
+      $scope.money -= $scope.funding.president;
+      $scope.profit =  $scope.money - lastMoney;
+      $scope.supply1 += parseInt($scope.retail.quantity1);
+      $scope.supply2 += parseInt($scope.retail.quantity2);
+      console.log("SUPPLY CHANGING BY " + $scope.retail.quantity1);
       if ($scope.money <= 0) {
         $scope.production.quantity1 = 0;
         $scope.production.quantity2 = 0;
         $scope.retail.quantity1 = 0;
         $scope.retail.quantity2 = 0;
       };
+      
+      $scope.ticksPassed += 1;
+      $scope.qualitySum += parseInt($scope.production.quality);
+      $scope.qualityAverage = parseFloat($scope.qualitySum / $scope.ticksPassed);
+    
+      if ($scope.qualityAverage > 60) {
+        console.log($scope.qualityAverage);
+        $scope.allowPresidencyRun = true;
+      } else {
+        $scope.allowPresidencyRun = false;
+      }
+
     };
+    
+    $scope.runForPresident = function() {
+      $scope.presidencyRun = true;
+      $scope.allowPresidencyRun = false;
+    } 
 
     $scope.updateQuantity = function () {
       if ($scope.production.quantity1 > $scope.getMaxProd1())
@@ -126,7 +172,7 @@ angular.module('LD34.game', ['ngRoute'])
 
 
     $scope.getBudgetUsed = function() {
-      return ($scope.getPrice1() * $scope.production.quantity1) + ($scope.getPrice2() * $scope.production.quantity2);
+      return parseInt($scope.funding.president) + ($scope.getPrice1() * $scope.production.quantity1) + ($scope.getPrice2() * $scope.production.quantity2);
     };
    
     $scope.buyButton1 = function() {
