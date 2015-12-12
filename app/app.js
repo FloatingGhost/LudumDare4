126824dc09d388202e8a13c93fd30d63bcd38ca1
Hannah Ward <Hannah.ward9001@gmail.com>
2015-12-12 04:14:57 +0000

    Graphs

--- app/app.js
+++ app/app.js
@@ -12,3 +12,16 @@ app.config(['$routeProvider', function($routeProvider) {
    $routeProvider.caseInsensitiveMatch = true;
 }]);
 
+app.directive('script', function() {
+    return {
+        restrict: 'E',
+        scope: false,
+        link: function(scope, elem, attr) {
+            if (attr.type === 'text/javascript-lazy') {
+                var code = elem.text();
+                var f = new Function(code);
+                f();
+            }
+        }
+    };
+});
--- app/game/game.js
+++ app/game/game.js
@@ -11,5 +11,16 @@ angular.module('LD34.game', ['ngRoute'])
 }])
 
 .controller('game-Ctrl', ['$scope', function($scope) {
-    }
+    $scope.money = 1000;  
+    $scope.button1stock = 5;
+    $scope.button2stock = 5;
+    $scope.button1cost = 50;
+    $scope.button2cost = 30;
+
+    $scope.buyButton1 = function() {
+      if ($scope.money > $scope.button1cost) {
+        $scope.money -= $scope.button1cost;
+      }
+    };
+  }
 ]);
