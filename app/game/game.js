e37cdaa8b6726adc3fb72fe5527269349ceae978
Hannah Ward <Hannah.ward9001@gmail.com>
2015-12-12 13:40:13 +0000

    Fixed being able to buy over supply limit

--- app/game/game.js
+++ app/game/game.js
@@ -11,7 +11,7 @@ angular.module('LD34.game', ['ngRoute'])
 }])
 
 .controller('game-Ctrl', ['$scope', "$interval", function($scope, $interval) {
-    $scope.money = 1000;  
+    $scope.money = 10000;  
     $scope.button1stock = 5;
     $scope.button2stock = 5;
     $scope.button1cost = 50;
@@ -27,6 +27,11 @@ angular.module('LD34.game', ['ngRoute'])
       quantity1: 5,
       quantity2: 3
     }
+
+    $scope.retail = {
+      quantity1: 5,
+      quantity2: 3
+    };
 
     $scope.update = function() {
       $scope.$broadcast("timer-stop");
@@ -46,16 +51,25 @@ angular.module('LD34.game', ['ngRoute'])
       $scope.priceArray1 = $scope.priceArray1.splice(1, $scope.dayMax);
       $scope.priceArray2 = $scope.priceArray2.splice(1, $scope.dayMax);
       
-      var newPrice1 = ($scope.priceArray1[$scope.dayMax - 1]) + (Math.random()*5*($scope.supplyRatio1 > 1? 1:-1));
-      var newPrice2 = ($scope.priceArray2[$scope.dayMax - 1]) + (Math.random()*5*($scope.supplyRatio1 > 1? 1:-1)); 
+      var newPrice1 = ($scope.priceArray1[$scope.dayMax - 1]) + (Math.random()*5*($scope.supplyRatio1 > 1? -1:1));
+      var newPrice2 = ($scope.priceArray2[$scope.dayMax - 1]) + (Math.random()*5*($scope.supplyRatio1 > 1? -1:1)); 
       if (newPrice1 <= 1) newPrice1 = 1;
       if (newPrice2 <= 1) newPrice2 = 1;
       $scope.priceArray1.push(newPrice1);
       $scope.priceArray2.push(newPrice2);
 
       //Number of buttons bought
+      var numberBought1 = Math.floor(0.2*Math.exp(Math.E, ($scope.supply1 - $scope.demand1)*Math.random()));
+      var numberBought2 = Math.floor(0.2*Math.exp(Math.E, ($scope.supply2 - $scope.demand2)*Math.random())); 
+      $scope.supply1 -= numberBought1;
+      $scope.supply2 -= numberBought2; 
       
-      
+      if ($scope.supply1 < 0) $scope.supply1 = 0;
+      if ($scope.supply2 < 0) $scope.supply2 = 0;
+      $scope.demand1 -= Math.floor(6*Math.random()*(Math.random()>0.5?1:-1))
+      $scope.demand2 -= Math.floor(6*Math.random()*(Math.random()>0.5?1:-1));
+      if ($scope.demand1 < 0) $scope.demand1 = 0;
+      if ($scope.demand2 < 0) $scope.demand2 = 0;
       var karr = [];
       for (i = 0; i < $scope.dayMax; i++) {
         karr.push(i + 1);
@@ -66,6 +80,21 @@ angular.module('LD34.game', ['ngRoute'])
                       xaxis: {title: "Days"},margin: {l: 40, r: 30,  b: 30, t: 30}};
       Plotly.newPlot("graph", [data, data2], layout);
 
+      //Manufacture buttons
+      $scope.button1stock += $scope.production.quantity1 - $scope.retail.quantity1;
+      $scope.button2stock += $scope.production.quantity2 - $scope.retail.quantity2;
+      $scope.money -= $scope.production.quantity1 * $scope.getPrice1();
+      $scope.money -= $scope.production.quantity2 * $scope.getPrice2();
+      $scope.money += $scope.retail.quantity1 * $scope.getPrice1();
+      $scope.money += $scope.retail.quantity2 * $scope.getPrice2();
+      $scope.supply1 += $scope.retail.quantity1;
+      $scope.supply2 += $scope.retail.quantity2;
+      if ($scope.money <= 0) {
+        $scope.production.quantity1 = 0;
+        $scope.production.quantity2 = 0;
+        $scope.retail.quantity1 = 0;
+        $scope.retail.quantity2 = 0;
+      };
     };
 
     $scope.updateQuantity = function () {
@@ -84,11 +113,15 @@ angular.module('LD34.game', ['ngRoute'])
     };
 
     $scope.getMaxProd1 = function() {
-      return Math.floor(($scope.money- $scope.production.quantity2*$scope.getPrice2())/$scope.getPrice1());
+      var x = Math.floor(($scope.money- $scope.production.quantity2*$scope.getPrice2())/$scope.getPrice1());
+      if (x <= 0) return 0; 
+      return x;
     };
 
     $scope.getMaxProd2 = function() {
-      return Math.floor(($scope.money - $scope.production.quantity1*$scope.getPrice1())/$scope.getPrice2());
+      var x = Math.floor(($scope.money - $scope.production.quantity1*$scope.getPrice1())/$scope.getPrice2());
+    if (x <= 0) return 0;
+    return x;
     };
 
 
@@ -97,7 +130,7 @@ angular.module('LD34.game', ['ngRoute'])
     };
    
     $scope.buyButton1 = function() {
-      if ($scope.money > $scope.priceArray1[$scope.dayMax]) {
+      if ($scope.supply1 > 0 && $scope.money > $scope.priceArray1[$scope.dayMax]) {
         $scope.money -= $scope.priceArray1[$scope.dayMax];
         $scope.button1stock+=1;
         $scope.supply1-=1;;
@@ -114,7 +147,7 @@ angular.module('LD34.game', ['ngRoute'])
     };
 
     $scope.buyButton2 = function() {
-      if ($scope.money > $scope.priceArray2[$scope.dayMax]) {
+      if ($scope.supply2 > 0 && $scope.money > $scope.priceArray2[$scope.dayMax]) {
         $scope.money -= $scope.priceArray2[$scope.dayMax];
         $scope.button2stock+=1;
         $scope.supply2-=1;
